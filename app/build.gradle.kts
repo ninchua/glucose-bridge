1a19b3f8af58609387b1e0b92d2ff85efb07a70f
ninchua <100162231+ninchua@users.noreply.github.com>
2025-12-15 15:23:24 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,17 +5,21 @@ plugins {
 
 android {
     namespace = "it.antonio.glucosebridge"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "it.antonio.glucosebridge"
         minSdk = 28
         targetSdk = 34
+
         versionCode = 1
         versionName = "1.0"
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
         release {
             isMinifyEnabled = false
         }
@@ -25,6 +29,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -32,7 +37,11 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+
+    // Health Connect
     implementation("androidx.health.connect:connect-client:1.1.0-alpha10")
+
+    // HTTP client
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
